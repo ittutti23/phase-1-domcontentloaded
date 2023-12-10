54a8a9ecd195ce293812f91f95ea76a4a3bb77d6
Peyton <1209942pl@gmail.com>
2023-12-10 11:21:08 -0500

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,14 +5,4 @@ describe( "index.js", () => {
     expect( document.querySelector( 'p' )
         .textContent )
       .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
-
-    const event = document.createEvent( 'Event' );
-    event.initEvent( 'DOMContentLoaded', true, true );
-    window.document.dispatchEvent( event );
-
-    expect( document.querySelector( 'p' )
-        .textContent )
-      .to.equal( "This is really cool!" );
-  } )
-
-} )
+  })})
